b5bd4b3de7c66eadda69587ca42a9721debe2b2e
gulshanbauddh <gulshanbncet@gmail.com>
2026-05-28 16:48:57 +0530

    refactor: remove unused variable and clean up code

--- practice1/addTwoNumber.java
+++ practice1/addTwoNumber.java
@@ -1,18 +1,17 @@
 package practice1;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class addTwoNumber{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int a,b,c,d;
+        int a,b,c;
         System.out.print("Enter Two number.\nEnter First Number");
         a=sc.nextInt();
         System.out.print("Enter Second Number");
         b=sc.nextInt();
         c=a+b;
         System.out.printf("a=%d and b=%d and Sum of a+b=%d",a,b,c);
-//        System.out.println(a+"+"+b+"="+c);
+        sc.close();
     }
 }
