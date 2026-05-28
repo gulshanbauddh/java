086b8f15e06f2336bcc2f0ef3530817629608e7c
gulshanbauddh <gulshanbncet@gmail.com>
2026-05-28 16:45:21 +0530

    through IDEA

--- practice1/AllDataTypesExample.java
+++ practice1/AllDataTypesExample.java
@@ -1,4 +1,17 @@
 package practice1;
 
 public class AllDataTypesExample {
+  public static void main(String[] args) {
+//    1. Primitive data type
+//      a. Integer Type
+    byte byteValue = (byte) 25;
+    short shortValue = 2554;
+    int intValue = Integer.MAX_VALUE;
+    long longValue = Long.MAX_VALUE;
+    float floatValue = Float.MAX_VALUE;
+    double doubleValue = Double.MAX_VALUE;
+    boolean boolValue = true;
+    char charValue = 'a';
+    System.out.println("byteValue: " +byteValue+ "\nSortValue: " + shortValue + "\nInt value: " + intValue+ "\nLong Value: " +longValue+ "\nFloat: Value: " +floatValue + "\nDouble Value: " +doubleValue + "\nBool value: " + boolValue + "\nChar Value: " + charValue);
+  }
 }
---
+++ practice1/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package practice1;
+
+class Employee {
+  private String name;
+  private int id;
+  private String department;
+  private double salary;
+  // Constructor
+  public Employee(String name, int id, String department, double salary) {
+    this.name = name;
+    this.id = id;
+    this.department = department;
+    this.salary = salary;
+  }
+  // Method to display employee details
+  public void displayDetails() {
+    System.out.println("Employee Details:");
+    System.out.println("Name: " + name);
+    System.out.println("ID: " + id);
+    System.out.println("Department: " + department);
+    System.out.println("Salary: $" + salary);
+  }
+}
+public class EmployeeDetails {
+  public static void main(String[] args) {
+// Creating an Employee object
+    Employee emp1 = new Employee("Gulshan", 894, "CSA", 98000);
+// Displaying the employee details
+    emp1.displayDetails();
+  }
+}
