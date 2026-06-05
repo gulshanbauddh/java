d0833fb3d980ee32e68b28a60b1020b0a7ad0949
gulshanbauddh <gulshanbncet@gmail.com>
2026-06-05 11:36:22 +0530

    Check Symmetric Matrix Done

---
+++ Nsti_Code/Symmetric_Matrix.java
@@ -0,0 +1,73 @@
+package Nsti_Code;
+
+import java.util.Scanner;
+
+// This is main Class.
+public class Symmetric_Matrix {
+  // Input Method
+  public static void inputMat(int m, int[][] mat) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("\nEnter Matrix elements:");
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.printf("mat [%d %d] = ", i + 1, j + 1);
+        mat[i][j] = sc.nextInt();
+      }
+    }
+  }
+
+  // Transpose Method
+  public static void transpose(int m,int[][]mat, int[][] trans) {
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < m; j++) trans[j][i] = mat[i][j];
+    }
+  }
+  // Display Method
+  public static void displayMat(int m, int[][]mat, int[][]trans) {
+    System.out.println("\nOriginal Input Matrix mat is:");
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.printf("%-5d", mat[i][j]);
+      }
+      System.out.println();
+    }
+    System.out.println("\nTranspose Matrix is:");
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.printf("%-5d", trans[i][j]);
+      }
+      System.out.println();
+    }
+  }
+  public static void checkSymmetric(int m,int[][] mat){
+    boolean isSymatric=true;
+    for (int i=0; i<m; i++){
+      for (int j=0; j<m; j++){
+        if (mat[i][j]!= mat[j][i]){
+          isSymatric=false;
+        }
+      }
+    }
+    System.out.println((isSymatric)?"\nMatrix is Symmetric.":"\nMatrix is not Symmetric.");
+  }
+  // Main Method
+  public static void main(String[] args) {
+    int m, n, p, q;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter row limit of Matrix A: ");
+    m = sc.nextInt();
+    System.out.print("Enter column limit of Matrix A: ");
+    n = sc.nextInt();
+    if (m==n) {
+      int[][] mat = new int[m][m];
+      int[][] trans = new int[m][m];
+      inputMat(m, mat);
+      transpose(m, mat, trans);
+      displayMat(m, mat, trans);
+      checkSymmetric(m,mat);
+    } else {
+      System.out.println("Rows and Column must be same.");
+    }
+  }
+
+}
---
+++ Nsti_Code/Transpose_Matrix.java
@@ -0,0 +1,56 @@
+package Nsti_Code;
+
+import java.util.Scanner;
+
+// This is main Class.
+public class Transpose_Matrix {
+  // Input Method
+  public static void inputMat(int m, int n, int[][] matA) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("\nEnter Matrix elements:");
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.printf("mat [%d %d] = ", i + 1, j + 1);
+        matA[i][j] = sc.nextInt();
+      }
+    }
+  }
+
+  // Transpose Method
+  public static void transpose(int m, int n,int[][]matA, int[][] trans) {
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) trans[j][i] = matA[i][j];
+    }
+  }
+  // Display Method
+  public static void displayMat(int m, int n, int[][]matA, int[][]trans) {
+    System.out.println("\nOriginal Input Matrix matA is:");
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.printf("%-5d", matA[i][j]);
+      }
+      System.out.println();
+    }
+    System.out.println("\nTranspose Matrix is:");
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.printf("%-5d", trans[i][j]);
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int m, n, p, q;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter row limit of Matrix A: ");
+    m = sc.nextInt();
+    System.out.print("Enter column limit of Matrix A: ");
+    n = sc.nextInt();
+      int[][] matA=new int[m][n];
+      int[][] trans=new int[n][m];
+      inputMat(m, n, matA);
+      transpose(m, n, matA, trans);
+      displayMat(m, n, matA, trans);
+  }
+}
