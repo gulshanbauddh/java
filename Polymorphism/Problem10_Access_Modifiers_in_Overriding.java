3b2a4762be451172f01cd40ef049fbf65be72a8b
gulshanbauddh <gulshanbncet@gmail.com>
2026-06-08 22:49:06 +0530

    Polymorphism all 10 program done.

--- Polymorphism/Problem10_Access_Modifiers_in_Overriding.java
+++ Polymorphism/Problem10_Access_Modifiers_in_Overriding.java
@@ -1,14 +1,30 @@
 package Polymorphism;
-class Parent2{
+
+class Parent2 {
   protected void show() {
     System.out.println("Inside Protected Parent Method");
   }
 }
-class Child2 extends Parent2{
 
+class Child2 extends Parent2 {
+  @Override
+//  void show(){ // Not Allow
+//    System.out.println("Inside Protected Child Method");
+//  }
+//  private void show(){ // Not Allow
+//    System.out.println("Inside Protected Child Method");
+//  }
+  protected void show() { // Allow
+    System.out.println("Inside Protected Child Method");
+  }
+//  void show(){ // Not Allow
+//    System.out.println("Inside Protected Child Method");
+//  }
 }
-public class Problem10_Access_Modifiers_in_Overriding {
-  public static void main(String[] args){
 
+public class Problem10_Access_Modifiers_in_Overriding {
+  public static void main(String[] args) {
+    Parent2 p1 = new Child2();
+    p1.show();
   }
 }
