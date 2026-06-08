32c47dae8afed283cb7fed2a24aec515baf7922f
gulshanbauddh <gulshanbncet@gmail.com>
2026-06-08 17:22:24 +0530

    somthing change

--- Polymorphism/Problem10_Access_Modifiers_in_Overriding.java
+++ Polymorphism/Problem10_Access_Modifiers_in_Overriding.java
@@ -1,4 +1,14 @@
 package Polymorphism;
+class Parent2{
+  protected void show() {
+    System.out.println("Inside Protected Parent Method");
+  }
+}
+class Child2 extends Parent2{
 
+}
 public class Problem10_Access_Modifiers_in_Overriding {
+  public static void main(String[] args){
+
+  }
 }
--- Polymorphism/Problem9_The_Final_Hurdle.java
+++ Polymorphism/Problem9_The_Final_Hurdle.java
@@ -1,4 +1,21 @@
 package Polymorphism;
-
+class Bike{
+  final void run() {
+    System.out.println("Bike is running safely at 60km/h.");
+  }
+}
+class Splendor extends Bike{
+//  @Override // This is not run give error due to final void run in Bike Parent class
+//  final void run(){
+//    System.out.println("Over Ride");
+//  }
+  void display(){
+    System.out.println("From Child Splendor");
+  }
+}
 public class Problem9_The_Final_Hurdle {
+  public static void main(String[] args){
+    Splendor b1=new Splendor();
+    b1.display();
+  }
 }
