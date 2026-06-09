fe0066bcf90c718cd701acbbefd07b4e18b6233c
gulshanbauddh <gulshanbncet@gmail.com>
2026-06-10 00:03:35 +0530

    odd number in reverse & sum

--- Nsti_Code/P_03_odd_number_in_reverse_and_sum.java
+++ Nsti_Code/P_03_odd_number_in_reverse_and_sum.java
@@ -3,15 +3,17 @@ package Nsti_Code;
 import java.util.Scanner;
 
 public class P_03_odd_number_in_reverse_and_sum {
-  public static void main(String[] args){
-    int num, sum=0;
-    Scanner sc= new Scanner(System.in);
+  public static void main(String[] args) {
+    int num, sum = 0;
+    Scanner sc = new Scanner(System.in);
     System.out.print("Enter any number: ");
-    num=sc.nextInt();
-    for(int i=num; i>0; i--){
-      System.out.print(i+" ");
-      sum+=i;
+    num = sc.nextInt();
+    for (int i = num; i > 0; i--) {
+      if (i % 2 != 0) {
+        System.out.print(i + " ");
+        sum += i;
+      }
     }
-    System.out.print("\nSum= "+sum);
+    System.out.print("\nSum= " + sum);
   }
 }
